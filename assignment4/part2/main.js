2b650751f1bd3adb22f36db422343b2f2722a285
Subeet <su6eet@gmail.com>
2024-04-01 18:04:39 -0400

    final commit

--- assignment4/part2/main.js
+++ assignment4/part2/main.js
@@ -38,10 +38,10 @@ btn.addEventListener('click', () => {
     if (btnClass === 'dark') {
       btn.setAttribute('class','light');
       btn.textContent = 'Lighten';
-      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+      overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     } else {
       btn.setAttribute('class','dark');
       btn.textContent = 'Darken';
-      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+      overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)';
     }
   });
